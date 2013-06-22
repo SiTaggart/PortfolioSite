9a372395905c4ecee65d7d3d12921bed0a86f9f2
Si Taggart <me@simontaggart.com>
2013-06-22 14:33:37 +1000

    rearrange the js files to be one minified file, and link to it

--- public/common/js/script.js
+++ public/common/js/script.js
@@ -18,53 +18,8 @@ Modernizr.load({
 //load utilities
 Modernizr.load({
 	load:[
-		'ielt9!/common/js/mylibs/selectivizr-min.js',
-		'common/js/plugins.js'
+		'ielt9!/common/js/mylibs/selectivizr-min.js'
 	]
-});
-
-st.siteInit = function(){
-
-	Modernizr.load({
-		load:[
-			'common/js/plugins/widget-twitterList.js',
-			'common/js/plugins/jquery.jFlickrFeed.js',
-			'common/js/plugins/widget-flickrList.js',
-			'common/js/plugins/widget-lastFmAlbum.js'
-		]
-	});
-
-}
-$(function(){
-	st.siteInit();
-
-	// initialize the plugin, pass in the class selector for the sections of content (blocks)
-	var scrollorama = $.scrollorama({ blocks:'.scrollblock', enablePin:false });
-	scrollorama.animate('#who .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#who h4',{ delay: 1200, duration: 400, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#move .scrollblock-fade',{ delay: 200, duration: 200, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#bio .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#stuff .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#example-dashfolio .scrollblock-fade',{ delay: 100, duration: 600, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#example-medibank .scrollblock-fade',{ delay: 100, duration: 600, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#example-pp2k .scrollblock-fade',{ delay: 100, duration: 600, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#example-myswitch .scrollblock-fade',{ delay: 100, duration: 600, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#example-mycoffee .scrollblock-fade',{ delay: 100, duration: 600, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#tweets .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#pics .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
-	scrollorama.animate('#albums .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
-
-	scrollorama.onBlockChange(function(){
-		if(scrollorama.blockIndex > 0) st.stickyLinks.show();
-		else st.stickyLinks.hide();
-	});
-
-	var waitForIt = setTimeout(function(){
-		$('#scroll-indication').addClass('go');
-	}, 5000);
-
-	st.stickyLinks.init();
-	st.setGreeting();
 });
 
 st.stickyLinks = {
@@ -98,22 +53,36 @@ st.setGreeting = function() {
 	else if (thehour >12) greeting = 'Afternoon';
 	else greeting = 'Morning';
 	$('header h1').text('Good ' + greeting);
-}
+};
 
+$(function(){
 
+	// initialize the plugin, pass in the class selector for the sections of content (blocks)
+	var scrollorama = $.scrollorama({ blocks:'.scrollblock', enablePin:false });
+	scrollorama.animate('#who .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#who h4',{ delay: 1200, duration: 400, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#move .scrollblock-fade',{ delay: 200, duration: 200, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#bio .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#stuff .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#example-dashfolio .scrollblock-fade',{ delay: 100, duration: 600, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#example-medibank .scrollblock-fade',{ delay: 100, duration: 600, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#example-pp2k .scrollblock-fade',{ delay: 100, duration: 600, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#example-myswitch .scrollblock-fade',{ delay: 100, duration: 600, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#example-mycoffee .scrollblock-fade',{ delay: 100, duration: 600, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#tweets .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#pics .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
+	scrollorama.animate('#albums .scrollblock-fade',{ delay: 300, duration: 400, property:'opacity', start:0, end:1 });
 
+	scrollorama.onBlockChange(function(){
+		if(scrollorama.blockIndex > 0) st.stickyLinks.show();
+		else st.stickyLinks.hide();
+	});
 
+	var waitForIt = setTimeout(function(){
+		$('#scroll-indication').addClass('go');
+	}, 5000);
 
+	st.stickyLinks.init();
+	st.setGreeting();
 
-
-
-
-
-
-
-
-
-
-
-
-
+});
