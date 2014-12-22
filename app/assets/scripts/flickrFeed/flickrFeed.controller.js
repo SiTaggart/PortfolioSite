e11b1cd0351d7e8fd45a50fced5b5130c80c20ae
Si Taggart <me@simontaggart.com>
2014-12-22 22:06:41 +1100

    Finish the flickr feed directive and service

--- app/assets/scripts/flickrFeed/flickrFeed.controller.js
+++ app/assets/scripts/flickrFeed/flickrFeed.controller.js
@@ -1,13 +1,34 @@
 ;(function () {
     'use strict';
 
-    var flickrFeedCtrl = function() {
-        var ctrl = this;
+    var flickrFeedCtrl = function flickrFeedCtrl($scope, FlickrFeedService) {
+        var ctrl = this,
+            DESIRED_NUMBER_OF_PHOTOS = 8;
 
         ctrl.test = 'Hello, World!';
+
+        FlickrFeedService.getFeed()
+            .then(handleRemoteData, handleError)
+            .finally(FlickrFeedService);
+
+        function handleRemoteData (data) {
+            var items = data.items.length;
+            data.items.splice(DESIRED_NUMBER_OF_PHOTOS, (items - DESIRED_NUMBER_OF_PHOTOS));
+            ctrl.items = data.items;
+        }
+
+        function handleError (response) {
+            // body...
+        }
+
+        function finalThing (argument) {
+            // body...
+        }
     };
 
-    angular.module('portfolio.flickrFeed.controller', [])
+    angular.module('portfolio.flickrFeed.controller', [
+            'portfolio.flickrFeed.service'
+        ])
         .controller('FlickrFeedCtrl', flickrFeedCtrl);
 
 }());
--- app/assets/scripts/flickrFeed/flickrFeed.directive.js
+++ app/assets/scripts/flickrFeed/flickrFeed.directive.js
@@ -1,19 +1,22 @@
 ;(function () {
     'use strict';
 
-    var flickrFeedDirective = function($rootScope) {
+    var flickrFeedDirective = function flickrFeedDirective($rootScope) {
         return {
             restrict: 'E',
             templateUrl: 'app/assets/scripts/flickrFeed/flickrFeed.tpl.html',
             replace: true,
+            controller: 'FlickrFeedCtrl as flickrFeedCtrl',
 
-            link: function(scope) {
+            link: function(scope, element, attr, ctrl) {
                 scope.someTest = "something news";
             }
         };
     };
 
-    angular.module('portfolio.flickrFeed.directive', [])
+    angular.module('portfolio.flickrFeed.directive', [
+            'portfolio.flickrFeed.controller'
+        ])
         .directive('flickrfeed', flickrFeedDirective);
 
 }());
