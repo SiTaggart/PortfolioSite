d6ac57cafeba004559d46bae80d3751ec183ac0a
Si Taggart <me@simontaggart.com>
2015-01-17 23:23:03 +1100

    Flickr feed tidy

--- app/assets/scripts/flickrFeed/flickrFeed.service.js
+++ app/assets/scripts/flickrFeed/flickrFeed.service.js
@@ -6,31 +6,28 @@
         this.getFeed = getFeed;
 
         function getFeed () {
-          var request = $http({
-            method: 'JSONP',
-            url: 'http://api.flickr.com/services/feeds/photos_public.gne?lang=en-us&format=json&jsoncallback=JSON_CALLBACK&id=51539284@N00#'
-          });
-          return( request.then( handleSuccess, handleError ) );
-        }
-
-        function handleError( response ) {
-          if (
-            ! angular.isObject( response.data ) ||
-            ! response.data.message
-            ) {
-            return( $q.reject( 'An unknown error occurred.' ) );
-          }
-          // Otherwise, use expected error message.
-          return( $q.reject( response.data.message ) );
+            var request = $http({
+                method: 'JSONP',
+                url: 'http://api.flickr.com/services/feeds/photos_public.gne?lang=en-us&format=json&jsoncallback=JSON_CALLBACK&id=51539284@N00#'
+            });
+            return( request.then( handleSuccess, handleError ) );
         }
 
         function handleSuccess( response ) {
-          return( response.data );
+            return( response.data );
+        }
+
+        function handleError( response ) {
+            if ( !angular.isObject( response.data ) || !response.data.message ) {
+                return( $q.reject( 'An unknown error occurred.' ) );
+            }
+            // Otherwise, use expected error message.
+            return( $q.reject( response.data.message ) );
         }
 
     };
 
     angular.module('portfolio.flickrFeed.service', [])
-        .service('FlickrFeedService', flickrFeedService);
+            .service('FlickrFeedService', flickrFeedService);
 
 }());
