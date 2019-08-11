cca0cbdd7dc9a01659c139d52ae9dc351d3e117d
Si Taggart <me@simontaggart.com>
2019-08-10 18:58:03 -0700

    chore: env variables?

--- api/tweets/index.js
+++ api/tweets/index.js
@@ -1,10 +1,10 @@
 import Twit from 'twit';
 
 const t = new Twit({
-  consumer_key: process.env.twitterKey,
-  consumer_secret: process.env.twitterSecret,
-  access_token: process.env.twitterToken,
-  access_token_secret: process.env.twitterTokenSecret
+  consumer_key: process.env.TWITTER_KEY,
+  consumer_secret: process.env.TWITTER_SECRET,
+  access_token: process.env.TWITTER_TOKEN,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET
 });
 
 export function handler(event, context, callback) {
