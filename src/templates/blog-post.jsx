6705bf6e4409fbe80b457a92251da9870929d409
Si Taggart <me@simontaggart.com>
2020-05-09 20:27:21 -0700

    fix: updates to react-helmet imports

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { graphql, Link } from 'gatsby';
 import get from 'lodash/get';
 import { SiteMain } from '../components/site-main';
