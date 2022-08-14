c60e61d86f3326e250b0c69a64cb13d78e1509af
Si Taggart <me@simontaggart.com>
2022-08-14 13:30:33 -0700

    fix: typescript cypress fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'cypress';
 
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
-    },
-  },
+  e2e: {},
 });
