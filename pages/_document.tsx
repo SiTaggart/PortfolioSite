1a130f120a44af6c36cff2efcf0d76a6d3f2d5d7
Si Taggart <me@simontaggart.com>
2023-02-05 15:35:24 -0800

    chore: switch to cloudflare analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Fira+Mono&display=swap"
             rel="stylesheet"
           />
-          <link crossOrigin="" href="https://www.google-analytics.com" rel="preconnect" />
         </Head>
         <body>
           <Main />
